e25f87210241b9acfd2601240a116bc55a016e3a
KarthicK Mariappan <karthick1.mariappan@gmail.com>
2017-05-15 09:13:36 +0530

    refactor: Remove unwanted jersey dependency.

--- src/main/java/io/karthick/springbootstarter/hello/HelloController.java
+++ src/main/java/io/karthick/springbootstarter/hello/HelloController.java
@@ -3,7 +3,6 @@
  */
 package io.karthick.springbootstarter.hello;
 
-import javax.ws.rs.GET;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
-	@GET
 	@RequestMapping("/hello")
 	public String sayHello(){
 		return "Hello";
